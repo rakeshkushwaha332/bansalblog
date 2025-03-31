8e47eb876fffe7d42fa37ea06a37a696ce7e5c54
Aman Verma <aksvbpl@gmail.com>
2025-03-31 19:22:16 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// Set EJS as the templating engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // Ensure views folder is correctly referenced
+app.use(express.static(path.join(__dirname, "public")));
+// Route
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+app.get("/contact", (req, res) => {
+	res.render("contact");
+});
+app.get("/sector", (req, res) => {
+	res.render("sector");
+});
+app.get("/about", (req, res) => {
+	res.render("about");
+});
+app.get('/team', (req, res) => {
+    res.render('team');
+  });
+app.get('/show', (req, res) => {
+    res.render('index');
+  });
+
+app.get('/service', (req, res) =>{
+    res.render('service');
+})
+app.listen(3000, () => {
+	console.log("Server is running on http://localhost:3000");
+});
